767a5ab129984f6b9baf90d3b5bbcfc95f8fa7c2
cs <csheehan630@gmail.com>
2023-07-21 02:27:06 -0500

    changes

--- lua/conacts/packer.lua
+++ lua/conacts/packer.lua
@@ -1,17 +1,15 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'  -- package manager
-	use 'github/copilot.vim'      -- copilot
+	use 'github/copilot.vim'      -- ai guy
 	use 'preservim/nerdtree'      -- nerding out
-	use { 'nvim-lua/plenary.nvim' }
-	use { 'ThePrimeagen/harpoon' }
+	use { 'nvim-lua/plenary.nvim' } -- async stuff (dependency)
+	use { 'ThePrimeagen/harpoon' } -- primetime
 	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } -- nvim treesitter
-	use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' }
+	use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' } -- find what u need
 	use({
 		"iamcco/markdown-preview.nvim",
 		run = function() vim.fn["mkdp#util#install"]() end,
